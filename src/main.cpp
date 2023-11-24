9b23a0c88d4cd881cf7f974da33cadb3ee65ca7d
coryjeacocke <68308072+coryjeacocke@users.noreply.github.com>
2023-11-24 12:15:56 +1100

    Updates.cpp
    Updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include <Arduino.h>
 
 #define THROTTLE_INPUT_PIN A0
-#define THROTTLE_OUTPUT_PIN A1
+// This is the first pin we found that would write analog properly
+#define THROTTLE_OUTPUT_PIN 3
 #define THROTTLE_MAX_POT_PIN A2
 #define THROTTLE_EXPONENT_POT_PIN A3
 
-#define THROTTLE_INPUT_MIN 0
-#define THROTTLE_INPUT_MAX 1023
+// These are the values we get between min and max on the throttle, which works out as 0.88-4.23v
+#define THROTTLE_INPUT_MIN 180
+#define THROTTLE_INPUT_MAX 866
+
+// Motor controller takes a 1-4v signal
+// Matching the throttle's 0.88-4.23v gives us 45-204 as 8 bits
+// 45 to 216 is gives the exact voltage output as we read from the throttle input
+#define THROTTLE_OUTPUT_MIN 45
+#define THROTTLE_OUTPUT_MAX 216
 
 #define THROTTLE_MAX_POT_MIN 0
 #define THROTTLE_MAX_POT_MAX 1023
@@ -14,7 +22,7 @@
 #define THROTTLE_EXPONENT_POT_MIN 0
 #define THROTTLE_EXPONENT_POT_MAX 1023
 
-#define THROTTLE_EXPONENT_MIN 0
+#define THROTTLE_EXPONENT_MIN 1
 #define THROTTLE_EXPONENT_MAX 10
 
 int exponentialMap(int x, int in_min, int in_max, int out_min, int out_max, double exponent);
@@ -36,7 +44,7 @@ void loop() {
   int throttleMax = analogRead(THROTTLE_MAX_POT_PIN);
 
   // Map throttleMax to the max motor speed
-  throttleMax = map(throttleMax, THROTTLE_MAX_POT_MIN, THROTTLE_MAX_POT_MAX, THROTTLE_INPUT_MIN, THROTTLE_INPUT_MAX);
+  throttleMax = map(throttleMax, THROTTLE_MAX_POT_MIN, THROTTLE_MAX_POT_MAX, THROTTLE_OUTPUT_MIN, THROTTLE_OUTPUT_MAX);
 
   // Read throttle input
   int throttleInput = analogRead(THROTTLE_INPUT_PIN);
@@ -46,10 +54,11 @@ void loop() {
   double throttleExponent = floatMap(throttleExponentPot, THROTTLE_EXPONENT_POT_MIN, THROTTLE_EXPONENT_POT_MAX, THROTTLE_EXPONENT_MIN, THROTTLE_EXPONENT_MAX);
 
   // Map throttle input to throttle output
-  int throttleOutput = exponentialMap(throttleInput, THROTTLE_INPUT_MIN, THROTTLE_INPUT_MAX, THROTTLE_INPUT_MIN, throttleMax, throttleExponent);
+  int throttleOutput = exponentialMap(throttleInput, THROTTLE_INPUT_MIN, THROTTLE_INPUT_MAX, THROTTLE_OUTPUT_MIN, throttleMax, throttleExponent);
 
   // Write throttle output to motor
   analogWrite(THROTTLE_OUTPUT_PIN, throttleOutput);
+  // analogWrite(THROTTLE_OUTPUT_PIN, throttleInput);
 
 #ifdef TEST_MODE
   drawMockThrottleCurve();
